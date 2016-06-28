210a6efcb513fb0d50892301bfdfceea6fb5ffe2
Jules Gosnell <Jules.x.Gosnell@barclayswealth.com>
2016-06-28 18:34:01 +0100

    explanatory text

--- src/killer/main.clj
+++ src/killer/main.clj
@@ -6,7 +6,7 @@
     [trades :as trades]]))
  
 (defn -main []
-  (server/add-model "Whales" whales/body (whales/start))
-  (server/add-model "Trades" trades/body (trades/start))
+  (server/add-model "Whales by species and totalled sightings." whales/body (whales/start))
+  (server/add-model "Trades by currency and totalled value" trades/body (trades/start))
   (server/start!))
 
