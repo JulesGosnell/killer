d5a08610bdb9966f742f359afbc9365201d97604
jules <jules_gosnell@yahoo.com>
2016-06-28 20:11:54 +0100

    reuse transducer code

--- src/killer/trades.clj
+++ src/killer/trades.clj
@@ -1,7 +1,8 @@
 (ns killer.trades
   (:require
    [clojure.core.async :as async  :refer (<! <!! >! >!! chan go go-loop)]
-   [hiccup.core        :as hiccup]))
+   [hiccup.core        :as hiccup]
+   [killer.transducers :refer :all]))
 
 (def body [:div {:data-chart-type "donut" :data-channel-name "trades"}])
 
@@ -13,24 +14,6 @@
 (defn random-trade [] {:currency (random-element currencies)
                        :value (random-element amounts)})
 
-(defn- esp-reduce
-  "a stateful transducer producing a running reduction of values seen"
-  [f i]
-  (fn [xf]
-    (let [accumulator (volatile! i)]
-      (fn
-        ([] (xf))
-        ([result] (xf result))
-        ([result input]
-         (vswap! accumulator (fn [a] (f a input)))
-         (xf result @accumulator))))))
-
-(defn esp-take-last
-  [n]
-  (esp-reduce
-   (fn [a v] (conj (if (= (count a) n) (pop a) a) v))
-   (clojure.lang.PersistentQueue/EMPTY)))
-
 (defn donut [s]
   [:killer/trades (mapv (fn [[k v]] {:label k :value v}) s)])
 
@@ -38,7 +21,7 @@
   (let [c (chan
          1024
          (comp
-          (esp-take-last 90)          ;sliding windows of last 90 events
+          (esp-take-last 90)       ;sliding window of last 90 events
           (map (fn [s] (group-by :currency s)))
           (map (fn [s] (map (fn [[k vs]] [(name k) (reduce (fn [a {v :value}] (+ a v)) 0 vs)]) s)))
           (map (fn [s] (sort-by first s))) ;sort frequencies by currency
--- src/killer/whales.clj
+++ src/killer/whales.clj
@@ -1,7 +1,8 @@
 (ns killer.whales
   (:require
    [clojure.core.async :as async  :refer (<! <!! >! >!! chan go go-loop)]
-   [hiccup.core        :as hiccup]))
+   [hiccup.core        :as hiccup]
+   [killer.transducers :refer :all]))
 
 (def body [:div {:data-chart-type "donut" :data-channel-name "whales"}])
 
@@ -29,24 +30,6 @@
 
 (defn random-element [s] (nth s (rand-int (count s))))
 
-(defn- esp-reduce
-  "a stateful transducer producing a running reduction of values seen"
-  [f i]
-  (fn [xf]
-    (let [accumulator (volatile! i)]
-      (fn
-        ([] (xf))
-        ([result] (xf result))
-        ([result input]
-         (vswap! accumulator (fn [a] (f a input)))
-         (xf result @accumulator))))))
-
-(defn esp-take-last
-  [n]
-  (esp-reduce
-   (fn [a v] (conj (if (= (count a) n) (pop a) a) v))
-   (clojure.lang.PersistentQueue/EMPTY)))
-
 (defn donut [s]
   [:killer/whales (mapv (fn [[k v]] {:label k :value v}) s)])
 
@@ -54,10 +37,10 @@
   (let [c (chan
          1024
          (comp
-          (esp-take-last 90)          ;sliding windows of last 90 events
-          (map frequencies)           ;aggregate whale frequencies
+          (esp-take-last 90)       ;sliding window of last 90 events
+          (map frequencies)          ;aggregate whale frequencies
           (map (fn [s] (sort-by first s))) ;sort frequencies by whale type
-          (map donut) ;reformat for front-end
+          (map donut)                      ;reformat for front-end
           )
          )]
   (future
